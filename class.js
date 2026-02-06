0e869b00e66570982b2afdd66205a00503bd6b53
gmnayem631 <gmnayem631@gmail.com>
2026-02-06 07:30:36 +0600

    learned class

--- class.js
+++ class.js
@@ -1,3 +1,14 @@
-class Player {}
-const player1 = new Player();
-console.log(player1);
+class Animal {
+  constructor(name, breed, age) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+
+  showAge() {
+    console.log(`This animal is ${this.age} years old`);
+  }
+}
+
+const animal1 = new Animal("Cruger", "German Shepherd", 3);
+animal1.showAge();
--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-console.log("Testing streak");
+const nums = [1, 2, 3, 4, 5];
+let output = nums.filter((n) => n % 2);
+console.log(output);
